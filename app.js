367a07e957188f7a7328c9887d87428fad1332ca
Saransh Gupta <saranshgupta1995@gmail.com>
2018-11-19 22:07:04 +0530

    heroku boilerplate

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http')
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+app.use(cors());
+
+app.get('*',(req,res)=>{
+    res.redirect('/');
+})
+
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('server is now listening'));
