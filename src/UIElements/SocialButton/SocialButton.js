a366f526cb5063df857d981300fa7a96a94c484a
Saransh Gupta <saransh.gupta@Saransh-Gupta-MAC.local>
2018-11-22 15:02:16 +0530

    change in social button styles

--- src/UIElements/SocialButton/SocialButton.js
+++ src/UIElements/SocialButton/SocialButton.js
@@ -4,19 +4,37 @@ import Styles from './SocialButton.module.scss';
 const SocialButton = (props) => {
 
     function randomizeOpacity() {
-        const buddies=document.getElementsByClassName('buddies')
-        const limit=40;
+        const buddies = document.getElementsByClassName('buddies')
+        const limit = 40;
 
-        for(let i=0;i<buddies.length;i++) {
-            buddies[i].style.opacity = Math.random()*0.8;
-            buddies[i].style.left = Math.random()*limit*[1,-1][~~(Math.random()*2)] + 'px';
-            buddies[i].style.top = Math.random()*limit*[1,-1][~~(Math.random()*2)] + 'px';            
+        for (let i = 0; i < buddies.length; i++) {
+            buddies[i].style.opacity = Math.random() * 0.8;
+            buddies[i].style.left = Math.random() * limit * [1, -1][~~(Math.random() * 2)] + 'px';
+            buddies[i].style.top = Math.random() * limit * [1, -1][~~(Math.random() * 2)] + 'px';
         };
+    }
+
+    function resetOpacity() {
+        const buddies = document.getElementsByClassName('buddies')
+
+        for (let i = 0; i < buddies.length; i++) {
+            buddies[i].style.opacity = 0;
+            buddies[i].style.left = 0 + 'px';
+            buddies[i].style.top = 0 + 'px';
+        };
+    }
+
+    function initEvent() {
+        const target = document.getElementsByClassName(Styles.SocialButton)[0];
+        target.style.border = 'none';
+        setTimeout(() => {
+            target.style.border = '2px solid seashell'
+        }, 300);
     }
 
     return (
 
-        <div onMouseEnter={randomizeOpacity} className={Styles.SocialButton}>
+        <div onClick={initEvent} onMouseLeave={resetOpacity} onMouseEnter={randomizeOpacity} className={Styles.SocialButton}>
             <div className="buddies"></div>
             <div className="buddies"></div>
             <div className="buddies"></div>
