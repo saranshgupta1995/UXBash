8267f5b3b774e73c6ce0ff9ed5800c3156a4db98
Saransh Gupta <saransh.gupta@Saransh-Gupta-MAC.local>
2018-11-22 11:23:41 +0530

    Basic Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,16 @@ import React from 'react';
 import Styles from './Header.module.scss';
 import AccessControl from '../test';
 
-const Header = (props) => ( 
-    <header className={Styles.Header}></header>
+const Header = (props) => (
+    <header className={Styles.Header}>
+        <div className="logo">Pika</div>
+        <ul className={Styles.Menu}>
+            <li>Pika</li>
+            <li>Pika Pika</li>
+            <li>Pika Pi</li>
+            <li>Pikachu</li>
+        </ul>
+    </header>
 )
 
- export default Header;
+export default Header;
